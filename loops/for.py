f59ae2a23fe31fdfc25105023663be8fdc9fdc79
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-12-17 13:40:29 +0530

    patterns

--- loops/for.py
+++ loops/for.py
@@ -81,3 +81,56 @@
 #     for col in range(1, num-row+1):
 #         print("* ", end="")
 #     print()
+
+
+# print(ord("A")) # 65
+# print(ord("Z")) # 90
+# print(ord("a")) # 97
+# print(ord("z")) # 122
+
+# print(chr(65)) # A
+# print(chr(122)) # z
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(chr(col + 64), end="")
+#     print()
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(chr(row + 96), end="")
+#     print()
+
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+
+# num = 5
+# global_var = 1
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(chr(global_var + 64)," ", end="")
+#         global_var += 1
+#     print()
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num+1):
+#         if(row == 1 or row == num or col == 1 or col == num):
+#             print("* ", end="")
+#         else:
+#             print("  ", end="")
+#     print()
+
+
+num = 15
+for row in range(1, num+1):
+    for col in range(1, num+1):
+        if(row % 2 == 0):
+            print("0 ", end="")
+        else:
+            print("1 ", end="")
+    print()
--- loops/patterns.py
+++ loops/patterns.py
@@ -108,3 +108,153 @@
 #     * * * * *
 #       * * *
 #         *
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(row, end="")
+#     print()
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# A
+# AB
+# ABC
+# ABCD
+# ABCDE
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(chr(col + 64), end="")
+#     print()
+
+# a
+# bb
+# ccc
+# dddd
+# eeeee
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(chr(row + 96), end="")
+#     print()
+
+1
+23
+456
+78910
+1112131415
+
+
+# A
+# B  C
+# D  E  F
+# G  H  I  J
+# K  L  M  N  O
+
+num = 5
+global_var = 1
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        print(chr(global_var + 64)," ", end="")
+        global_var += 1
+    print()
+
+# * * * * *
+# *       *
+# *       *
+# *       *
+# * * * * *
+
+num = 5
+for row in range(1, num+1):
+    for col in range(1, num+1):
+        if(row == 1 or row == num or col == 1 or col == num):
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print()
+
+# * * * * *
+#  *       *
+#   *       *
+#    *       *
+#     * * * * *
+
+#     * * * * *
+#    *       *
+#   *       *
+#  *       *
+# * * * * *
+
+# 01010
+# 10101
+# 01010
+# 10101
+# 01010
+
+num = 5
+flag = 0
+for row in range(1, num+1):
+    for col in range(1, num+1):
+        if(flag == 0):
+            print("0 ", end="")
+            flag = 1
+        else:
+            print("1 ", end="")
+            flag = 0
+    print()
+
+
+num = 15
+for row in range(1, num+1):
+    for col in range(1, num+1):
+        if(row % 2 == 0):
+            print("0 ", end="")
+        else:
+            print("1 ", end="")
+    print()
+
+num = 9
+
+# * * * * * * * * *
+# *   *   *   *   *
+# * * *   *   *   *
+# *       *   *   *
+# * * * * *   *   *
+# *           *   *
+# * * * * * * *   *
+# *               *
+# * * * * * * * * *
+# ----------------------
+# * 
+# *   
+# * * *   
+# *       
+# * * * * *   
+# *           
+# * * * * * * *   
+# *               
+# * * * * * * * * *
+# -----------------------
+# * * * * * * * * *
+#     *   *   *   *
+#     *   *   *   *
+#         *   *   *
+#         *   *   *
+#             *   *
+#             *   *
+#                 *
+#                 *
