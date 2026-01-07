ffd287cd3f04ddb457d623381ec2cc62a054a93f
Witzcode0 <brijesh.gondaliya07@gmail.com>
2026-01-07 13:35:10 +0530

    modules and packages | Part-2|

---
+++ modules_and_packages/main.py
@@ -0,0 +1,30 @@
+# import math
+
+# print((math.factorial(5)))
+
+# import random
+
+# print(random.random())
+# print(random.randint(1111,9999))
+
+# musics = ["m1", "m2", "m3", "m4", "m5"]
+# random.shuffle(musics)
+# print(musics)
+
+# import datetime
+
+# print(datetime.datetime.now())
+# print(datetime.datetime.now().date())
+# print(datetime.datetime.now().time())
+# print(datetime.datetime.now().weekday())
+# print(datetime.datetime.now().hour)
+# print(datetime.datetime.now().minute)
+# print(datetime.datetime.now().second)
+# print(datetime.datetime.now().day)
+# print(datetime.datetime.now().month)
+# print(datetime.datetime.now().year)
+
+
+# from datetime import datetime, timedelta
+# current_time = datetime.now()
+# print(current_time + timedelta(weeks=1, days=1,hours=1))
