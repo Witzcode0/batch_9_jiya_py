10a0addf6f569b450d23ce9cbab29531d0fa95ad
Witzcode0 <brijesh.gondaliya07@gmail.com>
2026-01-13 14:06:25 +0530

    regex

---
+++ advance_python/regex/main.py
@@ -0,0 +1,31 @@
+# https://docs.python.org/3/library/re.html
+
+import re
+
+# print(dir(re))
+
+# txt = "The rain in Spain"
+# x = re.search("^The.*Spain$", txt)
+
+# print(x.group())
+
+
+# data = "Ma map mat sun, moon, cat, watch match mall m mm mmm mmmm mmmmm mmmmmm"
+
+# pattern = r"\b[Mm]\w+"
+# res = re.findall(pattern, data)
+# print(res)
+
+
+# x = re.split(r"\s", data)
+# print(x)
+
+
+# data = "The kumbh mella will be conducted in surat"
+
+# print(re.sub("surat", "Allahbad", data))
+
+
+data = "frdsfdsfbgvrt brijesh@gmail.com retwfwet 2-01/2025 MoBilE +91 9898987633 ,yut jhugftiokf kuyhgofdillf ju brijesh.gondaliya@gmail.com ,jhtjskd ;   liedf oijh;oijfwe Mobile : 8980145007 koyd jh kj; mn;u whguul;  04/12/2025 ,ujhyfgoikufd uiph;if n brijesh-gondaliya@yahoo.in.co koyguhfd ;lkj ioj ds 23-0-2022 .,k uyik jhgl; mobile : 898-0145-007 "
+
+# find : email, mobile and date
